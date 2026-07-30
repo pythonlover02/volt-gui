c725e09d95b8f8ca15d1493ba51775fd64319e13
pythonlover02 <pythonloverv2@gmail.com>
2026-07-30 02:25:17 -0300

    [readme, make, volt, volt-gui] set things up for the new volt

--- src/about.py
+++
@@ -1,8 +0,0 @@
-def get_about_settings() -> dict:
-    return {
-        "_tab_metadata": (False,),
-        "description": "My AMD Adrenaline / NVIDIA Settings Linux Alternative",
-        "license": "GPL 3.0 License",
-        "author": "pythonlover02",
-        "version": "1.4.2"
-    }
